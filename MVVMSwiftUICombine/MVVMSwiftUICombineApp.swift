e5774326f5d57f8e825f325c00dae60552000c2c
Ashish Langhe <ashish.langhe@globant.com>
2023-11-03 14:54:21 +0530

    Initial Commit

---
+++ MVVMSwiftUICombine/MVVMSwiftUICombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMSwiftUICombineApp.swift
+//  MVVMSwiftUICombine
+//
+//  Created by Ashish Langhe on 03/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MVVMSwiftUICombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
